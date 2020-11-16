d05aff159a61af82fe718cf594539c9439dc3ec2
David Matevosyan <dav.matevosyan@gmail.com>
2020-11-16 11:39:20 +0400

    fix styleing

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
 const scores = document.getElementById('scores');
 
 const ground = new Image();
@@ -9,41 +8,42 @@ ground.src = 'img/bg.png';
 const mouse = new Image();
 mouse.src = 'img/mouse.png';
 
-const box = 50;
+let dir = '';
 let score = 0;
+const box = 50;
 
-let showMouse = {
+const showMouse = {
   x: Math.floor(Math.random() * 12 + 1) * box,
-  y: Math.floor(Math.random() * 10 + 1) * box
-}
+  y: Math.floor(Math.random() * 10 + 1) * box,
+};
 
-const snake = [{
-  x: 7 * box,
-  y: 5 * box
-}];
+const snake = [
+  {
+    x: 7 * box,
+    y: 5 * box,
+  },
+];
 
-let dir = ''
-
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', event => {
   event.preventDefault();
-  const { keyCode } = event;
-  if(keyCode == 37 && dir !== 'right') {
-    dir = 'left';
-  }
-  if(keyCode == 38 && dir !== 'down') {
-    dir = 'up';
-  }
-  if(keyCode == 39 && dir !== 'left') {
-    dir = 'right';
-  }
-  if(keyCode == 40 && dir !== 'up') {
-    dir = 'down';
+
+  switch (true) {
+    case event.keyCode === 37 && dir !== 'right':
+      return (dir = 'left');
+    case event.keyCode === 38 && dir !== 'down':
+      return (dir = 'up');
+    case event.keyCode === 39 && dir !== 'left':
+      return (dir = 'right');
+    case event.keyCode === 40 && dir !== 'up':
+      return (dir = 'down');
+    default:
+      return;
   }
 });
 
 function eatTail(head, arr) {
-  for(let i = 0; i < arr.length; i++) {
-    if(head.x === arr[i].x && head.y === arr[i].y) {
+  for (let i = 0; i < arr.length; i++) {
+    if (head.x === arr[i].x && head.y === arr[i].y) {
       clearInterval(gameInterval);
     }
   }
@@ -53,9 +53,9 @@ function drawGame() {
   ctx.drawImage(ground, 0, 0);
   ctx.drawImage(mouse, showMouse.x, showMouse.y);
 
-  for(let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = i === 0 ? 'blue': 'green';
-    ctx.fillRect(snake[i].x, snake[i].y, box, box)
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i === 0 ? 'blue' : 'green';
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
   }
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -63,36 +63,41 @@ function drawGame() {
   if (snakeX === showMouse.x && snakeY === showMouse.y) {
     score++;
     scores.innerHTML = score;
-    showMouse = {
-      x: Math.floor(Math.random() * 12 + 1) * box,
-      y: Math.floor(Math.random() * 10 + 1) * box
-    };
+    showMouse.x = Math.floor(Math.random() * 12 + 1) * box;
+    showMouse.y = Math.floor(Math.random() * 10 + 1) * box;
   } else {
-    snake.pop()
+    snake.pop();
   }
 
-  if(snakeX  < box || snakeX > box * 12 || snakeY < box || snakeY > box * 10) {
+  if (snakeX < box || snakeX > box * 12 || snakeY < box || snakeY > box * 10) {
     clearInterval(gameInterval);
+    alert('you lose');
+    window.location.reload();
   }
 
-  if(dir === 'left') {
-    snakeX -= box;
+  switch (dir) {
+    case 'left':
+      snakeX -= box;
+      break;
+    case 'right':
+      snakeX += box;
+      break;
+    case 'up':
+      snakeY -= box;
+      break;
+    case 'down':
+      snakeY += box;
+      break;
+    default:
+      break;
   }
-  if(dir === 'right') {
-    snakeX += box;
-  }
-  if(dir === 'up') {
-    snakeY -= box;
-  }
-  if(dir === 'down') {
-    snakeY += box;
-  }
+
   const newHead = {
     x: snakeX,
-    y: snakeY
-  }
+    y: snakeY,
+  };
 
-  eatTail(newHead, snake)
+  eatTail(newHead, snake);
 
   snake.unshift(newHead);
 }
